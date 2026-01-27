7d764a6289eae5694713af7515d4d7efeb9a3b97
Town <2410263032@mails.szu.edu.cn>
2026-01-27 13:04:29 +0800

    new

---
+++ README.md
@@ -0,0 +1,12 @@
+# Prompt4RE
+
+## Requirements
++ Python 3.10.18
++ `pip install -r requirements.txt`
+
+## Run
+To run the experiments, use the following command:
+
+```bash
+python PromptLearning.py --task crime
+```
---
+++ parse_args.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--task', default="crime", help="task name, can be crime or checkIn or serviceCall")
+parser.add_argument('--text_output_dim',  type=int,   default=144)
+parser.add_argument('--lr',  type=float,  default=0.0001)
+parser.add_argument('--epochs',         type=int,    default=2000)
+parser.add_argument('--model',     type=str,    default="llama")
+
+args = parser.parse_args()
